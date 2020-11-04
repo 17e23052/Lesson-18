1b69bf12396fa263a618ef206c6afa7d616f81ba
Mathman <17e23052@marling.gloucs.sch.uk>
2020-11-04 10:34:12 +0000

    Highest number calc.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#This calculates the average value of three numbers that are inputted by the user.
-def calculate(a,b,c):
-  answer = (a + b + c)/3
-  print(f"The average value of these numbers is {answer}")
+#This finds the highest number out of two numbers that are inputted by the user.
+def calculate(a,b):
+  if a == b:
+    print("The numbers are the same")
+  elif a > b:
+    highest_num = a
+  elif b > a:
+    highest_num = b
+  if a > b or b > a:
+    print(f"The highest number entered is {highest_num}")
 
 print("Enter the first number:")
 num1 = int(input())
 print("Enter the second number:")
 num2 = int(input())
-print("Enter the third number:")
-num3 = int(input())
 
-calculate(num1,num2,num3)
+calculate(num1,num2)
