c82214c8a5ed048709c323e0cf759f7e7f090a51
Mathman <17e23052@marling.gloucs.sch.uk>
2020-12-15 10:27:05 +0000

    Calculation checker

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-#This multiplies two numbers that are inputted by the user.
-def calculate(a,b):
-  answer = a * b
-  print(f"{a} * {b} = {answer}")
+def add(a,b):
+  add = a + b
+  print(f"{a} + {b} = {add}")
+def sub(a,b):
+  sub = a - b
+  print(f"{a} - {b} = {sub}")
+def mul(a,b):
+  mul = a * b
+  print(f"{a} * {b} = {mul}")
+def div(a,b):
+  div = a / b
+  print(f"{a} / {b} = {div}")
 
-print("Enter the first number:")
+print("Enter a number:")
 num1 = int(input())
-print("Enter the second number:")
+print("Enter another number:")
 num2 = int(input())
-
-calculate(num1,num2)
+print("Enter +, -, * or /:")
+print("If using divison, remainders will not be shown.")
+operation = input()
+if operation == "+":
+  add(num1,num2)
+elif operation == "-":
+  sub(num1,num2)
+elif operation == "*":
+  mul(num1,num2)
+elif operation == "/":
+  div(num1,num2)
+else:
+  print("Invalid operation. Please start program")
